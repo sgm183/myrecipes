c5f426649916a1d120b4b89e45c624460a763bd8
Steve <steve@verbdb.com>
2017-02-24 18:45:23 -0600

    initial build homepage and add bootstrap

---
+++ test/integration/pages_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class PagesTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get pages_home_url
+    assert response :success
+  end
+  test "should get root" do
+    get root_url
+    assert response :success
+  end
+end
